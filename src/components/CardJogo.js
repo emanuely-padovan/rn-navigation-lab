910778e60ffd7e05063e397d3340aaabcebc5452
emanuely-padovan <emanuely.padovan@aluno.senai.br>
2026-05-30 14:44:50 -0300

    Update CardJogo to use props and add styling
    Refactor CardJogo component to accept props and apply styles.

--- src/components/CardJogo.js
+++ src/components/CardJogo.js
@@ -1,13 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 // Passe os parametros de forma correta e realize a estilização do componente
-export default function CardJogo() {
+export default function CardJogo(props) {
   return (
-    <View>
-      <Text>Titulo</Text>
-      <Text>Genero</Text>
-      <Text>Plataforma</Text>
-      <Text>Nota</Text>
+    <View style={styles.card}>
+      <Text style={styles.titulo}>Titulo: {props.titulo}</Text>
+      <Text style={styles.info}>Genero: {props.genero}</Text>
+      <Text style={styles.info}>Plataforma: {props.plataforma}</Text>
+      <Text style={styles.info}>Nota: {props.nota}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgorundColor: "#fff".
+    borderRadius: 10,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "#e5e7eb",
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  info: {
+    fontSize: 10,
+  }
+})
